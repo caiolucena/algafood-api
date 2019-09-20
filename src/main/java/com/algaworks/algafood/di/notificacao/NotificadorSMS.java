116c11627e975691e1e7359a17d770c43f884d32
Caio Lucena <newsoft.dev01@gmail.com>
2019-09-20 17:06:11 -0300

    Aula 2.16. Ambiguidade de beans e injeção de lista de beans.

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.stereotype.Component;
+import com.algaworks.algafood.di.modelo.Cliente;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+  private boolean caixaAlta;
+
+  public NotificadorSMS() {
+    System.out.println("Notificador Email");
+  }
+
+  @Override
+  public void notificar(Cliente cliente, String mensagem) {
+
+    if (this.caixaAlta) {
+      mensagem = mensagem.toUpperCase();
+    }
+
+    System.out.printf("Notificando %s através do celular %s: %s\n", cliente.getNome(),
+        cliente.getTelefone(), mensagem);
+
+  }
+
+  public void setCaixaAlta(boolean caixaAlta) {
+    this.caixaAlta = caixaAlta;
+  }
+
+}
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -9,12 +10,16 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 public class AtivacaoClienteService {
 
   @Autowired(required = false)
-  private Notificador notificador;
+  private List<Notificador> notificadores;
 
   public void ativar(Cliente cliente) {
     cliente.ativar();
 
-    notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    for (Notificador notificador : notificadores) {
+
+      notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+
   }
 
 }
